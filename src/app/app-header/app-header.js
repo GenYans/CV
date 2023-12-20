78a9bd48c5cda86f664428bd0c02d547e46f43e7
GenYans <genapiter@yandex.ru>
2023-12-20 15:24:05 +0300

    Step 17 20.12.2023

--- src/app/app-header/app-header.js
+++ src/app/app-header/app-header.js
@@ -30,75 +30,75 @@ import Logo from '../../resources/icons/logo.svg';
                             <nav className="main-menu">
                             <ul className={nav ? ['list-inline', 'active'].join(' '): ['list-inline']}>
                                 <li className='list-inline-item'>
-                                    <Link 
+                                    <Link onClick={() => setNav(!nav)}
                                         activeClass='active'
                                         to="hello"
                                         spy={true}
                                         smooth={true}
                                         offset={-110}
-                                        duration={500}
+                                        duration={1000}
                                     >
                                     {t("hello")}
                                     </Link>
                                 </li>
                                 <li className='list-inline-item'>
-                                    <Link 
+                                    <Link onClick={() => setNav(!nav)}
                                     to="skills"
                                     spy={true}
                                     smooth={true}
                                     offset={-110}
-                                    duration={500}
+                                    duration={1000}
                                     >
                                     {t("skills")}
                                     </Link>
                                 </li>
                                 <li className='list-inline-item'>
-                                    <Link 
+                                    <Link onClick={() => setNav(!nav)}
                                     to="experience"
                                     spy={true}
                                     smooth={true}
                                     offset={-110}
-                                    duration={500}
+                                    duration={1000}
                                     >
                                     {t("experience")}
                                     </Link>
                                 </li>
                                 <li className='list-inline-item'>
-                                    <Link 
+                                    <Link onClick={() => setNav(!nav)}
                                     to="education"
                                     spy={true}
                                     smooth={true}
                                     offset={-110}
-                                    duration={500}
+                                    duration={1000}
                                     >
                                     {t("education")}
                                     </Link>
                                 </li>
                                 <li className='list-inline-item'>
-                                    <Link
+                                    <Link onClick={() => setNav(!nav)}
                                     to="portfolio"
                                     spy={true}
                                     smooth={true}
                                     offset={-110}
-                                    duration={500}
+                                    duration={1000}
                                     >
                                     {t("portfolio")}
                                     </Link>
                                 </li>
                                 <li className='list-inline-item'>
-                                    <Link 
+                                    <Link onClick={() => setNav(!nav)}
                                     to="contact"
                                     spy={true}
                                     smooth={true}
                                     offset={-110}
-                                    duration={500}
+                                    duration={1000}
                                     >
                                     {t("contacts")}
                                     </Link>
                                 </li>
                             </ul>
                             <div onClick={() => setNav(!nav)} className='mobile-btn'>
-                                {nav ? <AiOutlineClose size={35}/> : <AiOutlineMenu size={35}/>}
+                                {nav ? <AiOutlineClose size={35} style={{ zIndex:`10` }} color='white'/> : <AiOutlineMenu size={35}/>}
                             </div>
                         </nav>
 
